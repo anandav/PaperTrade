6c8379401d5b3a11ab2580940b8f5af16833da41
aditi <anand.av@hotmail.com>
2024-10-09 00:02:35 +0530

    added swagger

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,9 +16,9 @@
     >
       <div class="container mx-auto">
         <router-link to="/" class="pl-5">Paper Trade</router-link>
-        <!-- <router-link to="/builder" class="pl-5"
+        <router-link to="/builder" class="pl-5"
           > Place holder</router-link
-        > -->
+        >
         <!-- <router-link to="/about" class="pl-5">About</router-link> -->
         <div class="float-right">
           <label class="mr-3">
--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,14 @@ const logger = require("./common/logs");
 const port = process.env.PORT || 9090;
 const enable_dataapi = process.env.ENABLE_DATAAPI || "true";
 const conn_string = process.env.DBCONNECTIONSTRING;
-console.log("Connection string", conn_string);
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+console.log("Connection string", conn_string);
 // logger.info("port:", port);
 app.use(express.json());
 app.use(cors());
@@ -20,17 +26,10 @@ app.use(helmet());
 app.use("/strategy", strategyController);
 app.use("/portfolio", portfolioCotroller);
 app.use("/trade", tradeController);
-var options = {
-  index: "index.html"
-};
 
-//  app.use("/", options);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-// app.use("/", express.static('public'));
-
-//app.use(express.urlencoded({extended : true}));
 
 if (enable_dataapi == 'true') {
   app.use("/data", dataProvider);
@@ -39,31 +38,18 @@ if (enable_dataapi == 'true') {
     res.status(404).json({ "error": "Data endpoint is disabled" })
   });
 }
+
 if (conn_string) {
   mongoose
-  .connect(conn_string, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-  // mongoose.connect(
-  //   conn_string,
-  //   { useNewUrlParser: true, useUnifiedTopology: true },
-  //   (x) => {
-  //     if (x) {
-  //       logger.error(x);
-  //     }
-  //     else {
-  //       logger.info("DB connected...");
-  //     }
-  //   }
-  // );
-  
-
+  .connect(conn_string, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => logger.info("MongoDB connected"))
+  .catch((err) => logger.error(err));
 } else {
   logger.error("Empty connnection string!")
 }
 
 app.listen(port, (x) => {
-  logger.info("Service Started...");
+  logger.info("Service Started");
 });
 
 
