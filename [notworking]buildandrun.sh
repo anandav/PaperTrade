fb71171d0f550f336175e8bd5745416d162e846f
Anand A Venkatraman <anand.av@hotmail.com>
2025-03-31 00:23:24 +0530

    Renamed buil bash file

---
+++ [notworking]buildandrun.sh
@@ -0,0 +1,10 @@
+docker build -t ptserver -f ./server/Dockerfile  .
+docker build -t ptclient -f ./client/Dockerfile  .
+docker run -dp :9090:9090 ptserver
+docker run -dp :8080:8080 ptclient
+
+# docker pull mongodb/mongodb-community-server:latest
+# docker run -dp 27017:27017 mongodb/mongodb-community-server
+
+
+
