c4d6328791ad02d24c784e6241e4074061671c68
anandav <anand.av@hotmail.com>
2025-07-14 13:16:34 +0530

    Added swagger to servic

---
+++ server/src/controller/authcontroller.js
@@ -0,0 +1,32 @@
+
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).send({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(400).send({ error: 'Username already exists' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).send({ error: 'Login failed!' });
+    }
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      return res.status(401).send({ error: 'Login failed!' });
+    }
+    const token = jwt.sign({ _id: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send({ error: 'Login failed!' });
+  }
+};
