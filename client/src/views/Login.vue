2eaeee804d809386e7b015402ccba401357671be
anandav <anand.av@hotmail.com>
2025-10-12 17:04:11 +0530

    feat: update login view to use dynamic API URL and remove reset password link

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -21,9 +21,7 @@
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
             Sign In
           </button>
-          <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="http://localhost:9090/auth/b2c/resetpassword">
-            Forgot Password?
-          </a>
+         
         </div>
 
         <div class="flex items-center justify-between mt-4">
@@ -64,7 +62,7 @@ export default {
     },
 
     b2cLogin() {
-      window.location.href = 'http://localhost:9090/auth/b2c/login';
+      window.location.href = window.APP_CONFIG.API_URL + 'auth/b2c/login';
     }
   }
 };
